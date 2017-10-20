6ec436c72f1058a344020c972d7ddb567a6b1f0a
Márvio André Bezerra Silverio <marvio.bezerra@hotmail.com>
2017-10-20 17:00:57 -0400

    Sample change

--- eWorkshop.Database/Sample/Tables/SampleTable.sql
+++ eWorkshop.Database/Sample/Tables/SampleTable.sql
@@ -1,6 +1,9 @@
 ﻿CREATE TABLE [Sample].[SampleTable] (
     [IdSample] UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
     [Value]    VARCHAR (MAX)    NULL,
+    [Valid]    BIT              DEFAULT ((1)) NOT NULL,
     PRIMARY KEY CLUSTERED ([IdSample] ASC)
 );
+
+
 
