4a0f208fb8e3f075fe7af20501609733389feb07
ZeusAC <fabiociconi@yahoo.com.br>
2017-11-05 20:22:26 -0200

    05-11-2017

--- eWorkshop.Web/app/modules/public/account/sign-up.component.ts
+++ eWorkshop.Web/app/modules/public/account/sign-up.component.ts
@@ -3,15 +3,9 @@ import { FormGroup, Validators } from "@angular/forms";
 import { AutoFormService, EntityAction } from "xcommon";
 import { MatSnackBar } from "@angular/material/snack-bar";
 
-
-//imports -- Project
+//imports from our Project
 import { ISignUpEntity, Guid, RoleType } from "../../../entity";
 import { AuthService } from "../../service";
-
-
-
-//TODO:
-
 
 @Component({
 	selector: "sign-up",
@@ -20,20 +14,12 @@ import { AuthService } from "../../service";
 })
 export class SignUpComponent implements OnInit
 {
-	
+
 	private snackBar: MatSnackBar;
-
 	public Ready = false;
-
 	public ShowMessage = false;
 	public Message = "";
-
-	//private Role : RoleType;
-
-
 	public SignUpForm: FormGroup;
-
-
 
 	constructor(/*xcommon service*/	private autoFormService: AutoFormService,/*servico nosso*/private authService: AuthService) { }
 
@@ -45,36 +31,21 @@ export class SignUpComponent implements OnInit
 
 	}
 
-
 	public BuildForm(entity: ISignUpEntity): void
 	{
 
 		const autoForm = this.autoFormService.createNew<ISignUpEntity>();
 		/*Password: string;/PasswordConfirm: string;/RoleType: RoleType;/IdPerson: string;
-		/FirstName: string;/LastName: string;/Telephone: string;/Email: string;/Action: EntityAction;*/		
+		/FirstName: string;/LastName: string;/Telephone: string;/Email: string;/Action: EntityAction;*/
 		this.SignUpForm = autoForm
 			.AddValidator(c => c.FirstName, Validators.required)
 			.AddValidator(c => c.LastName, Validators.required)
 			.AddValidator(c => c.Telephone, Validators.required)
 			.AddValidator(c => c.Email, Validators.compose([Validators.email, Validators.required]))
+			//TODO: Custom validator to check passwords
 			.AddValidator(c => c.Password, Validators.required)
-			.AddValidator(c => c.PasswordConfirm, Validators.required)			
-			//.AddValidator(c => c.PasswordConfirm, Validators.bind([Validators.required, this.checkIfMatchingPasswords("Password", "PasswordConfirm")]))
+			.AddValidator(c => c.PasswordConfirm, Validators.required)
 			.Build(entity);
-		/*	.Build({
-				IdPerson: Guid.NewGuid()	,
-				FirstName: "",
-				LastName: "",
-				Telephone: "",
-				Email: "",
-				Password: "",
-				PasswordConfirm: "",
-				//conferir estes dois
-				RoleType: this.Role.Customer,
-				Action: EntityAction.New
-
-			});*/
-		//console.log("Role Type 1 :" + this.Role.Customer);
 		this.Ready = true;
 	}
 
@@ -92,22 +63,17 @@ export class SignUpComponent implements OnInit
 			PasswordConfirm: "",
 			Action: EntityAction.New,
 			RoleType: RoleType.Customer
-			//RoleType: this.Role.Customer,
 		});
-		console.log("Action      :" + EntityAction.New);
-		console.log("Role Type 2 :" + RoleType.Customer);
-		console.log("this.authService.Role;  " + this.authService.Role);
-	
 	}
+
+	//call service to save customer's information
 	public SignUp(entity: ISignUpEntity): void
 	{
 		//o que irei mandar para o service/.../
 		console.log(JSON.stringify(entity));
-		//
 
-		
-
-		this.authService.SignUp(entity)		
+		//			
+		this.authService.SignUp(entity)
 			.subscribe(res =>
 			{
 
@@ -124,21 +90,5 @@ export class SignUpComponent implements OnInit
 				//this.BuildForm(res.Entity.Token);
 			});
 		//return entity = this.authService.SignUp((entity: ISignUpEntity):Observable<IExecute<ITokenEntity>>);
-	}
-
-
-	private checkIfMatchingPasswords(password: string, passwordConfirmation: string)
-	{
-		return (group: FormGroup) =>
-		{
-			let passwordInput = group.controls[password],
-				passwordConfirmationInput = group.controls[passwordConfirmation];
-			if (passwordInput.value !== passwordConfirmationInput.value) {
-				return passwordConfirmationInput.setErrors({ notEquivalent: true })
-			}
-			else {
-				return passwordConfirmationInput.setErrors(null);
-			}
-		}
 	}
 }
