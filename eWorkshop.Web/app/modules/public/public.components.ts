d11a18b580d7b77436a2159bd013482d0ab7e109
rcarneir <rcarneir@my.centennialcollege.ca>
2017-11-10 01:24:34 -0500

    Create about component Register route to about component Created home page Adjust in the form layouts

--- eWorkshop.Web/app/modules/public/public.components.ts
+++ eWorkshop.Web/app/modules/public/public.components.ts
@@ -1,11 +1,13 @@
 import { LayoutPublicComponent } from "./layout";
 import { HomePublicComponent } from "./home";
+import { AboutComponent } from "./about";
 import { SignInComponent, SignUpComponent, SignUpWorkshopComponent } from "./account";
 
 
 export const PublicComponents = [
 	LayoutPublicComponent,
 	HomePublicComponent,
+	AboutComponent,
 	SignInComponent,
 	SignUpComponent,
 	SignUpWorkshopComponent
