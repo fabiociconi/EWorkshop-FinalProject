6fdddfa16c15ff3da01a7a63d187eab42763cd74
Márvio André Bezerra Silverio <marvio.bezerra@hotmail.com>
2017-12-13 20:11:05 -0500

    Added map on search workshop result

--- eWorkshop.Web/app/modules/customer/service/search.component.ts
+++ eWorkshop.Web/app/modules/customer/service/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from "@angular/core";
 import { CustomerService } from "../../service";
 import { IWorkshopsEntity, IWorkshopsFilter, ICoordinates } from "../../../entity";
 
@@ -9,34 +9,48 @@ import { IWorkshopsEntity, IWorkshopsFilter, ICoordinates } from "../../../entit
 })
 export class SearchComponent implements OnInit {
 
-	public ClientPosition: Position;
 	public Workshops: IWorkshopsEntity[] = [];
+
+	public lat: number = 0;
+	public lng: number = 0;
+	public zoom: number = 4;
+	public MapSize: number = 0;
+
+	@ViewChild('divMap') public DivMap: ElementRef;
 
 	constructor(private customerService: CustomerService) { }
 
 	public ngOnInit(): void {
 
-		if (window.navigator && window.navigator.geolocation) {
-			window.navigator.geolocation.getCurrentPosition(
+		setTimeout(() => this.OnResize(null), 100);
+		this.SetCurrentPosition();
+	}
+
+	@HostListener('window:resize', ['$event'])
+	public OnResize(event: Event): void {
+		this.MapSize = this.DivMap.nativeElement.offsetHeight;
+	}
+
+	private SetCurrentPosition() {
+		if ("geolocation" in navigator) {
+			navigator.geolocation.getCurrentPosition(
 				position => {
-					this.ClientPosition = position;
+					this.lat = position.coords.latitude;
+					this.lng = position.coords.longitude;
+					this.zoom = 4;
 					this.Filter();
 				},
 				error => {
 					this.Filter();
-				}
-			);
-		};
+				});
+		}
 	}
 
 	private Filter(): void {
 		const filter: IWorkshopsFilter = {
-			MaximumDistance: 134
-		};
-
-		if (this.ClientPosition) {
-			filter.ClientLatitude = this.ClientPosition.coords.latitude;
-			filter.ClientLongitude = this.ClientPosition.coords.longitude;
+			MaximumDistance: 134,
+			ClientLatitude: this.lat,
+			ClientLongitude: this.lng
 		};
 
 		this.customerService.Search(filter)
